31239b4d2eb3e962db4fd82ea7f6297a0d4bc8cb
scccy <shici2880@gmail.com>
2025-01-26 21:59:51 +0800

    数据union Demo

--- src/main/java/combine/UnionDemo.java
+++ src/main/java/combine/UnionDemo.java
@@ -15,9 +15,9 @@ public class UnionDemo {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        DataStreamSource<Integer> source1 = env.fromElements(1, 2, 3);
-        DataStreamSource<Integer> source2 = env.fromElements(11, 22, 33);
-        DataStreamSource<String> source3 = env.fromElements("111", "222", "333");
+        DataStreamSource<Integer> source1 = env.fromData(1, 2, 3);
+        DataStreamSource<Integer> source2 = env.fromData(11, 22, 33);
+        DataStreamSource<String> source3 = env.fromData("111", "222", "333");
 
         /**
          * TODO union：合并数据流
@@ -25,7 +25,7 @@ public class UnionDemo {
          * 2、 一次可以合并多条流
          */
 //        DataStream<Integer> union = source1.union(source2).union(source3.map(r -> Integer.valueOf(r)));
-        DataStream<Integer> union = source1.union(source2, source3.map(r -> Integer.valueOf(r)));
+        DataStream<Integer> union = source1.union(source2, source3.map(Integer::valueOf));
         union.print();
 
 
